2bef83b9a62bd68cda075bd2a76fdc2811c44d5d
anamartinez97m <anamartinez97m@gmail.com>
2022-02-15 20:49:45 +0100

    update get recipes to allow filtering

--- app/controllers/RecipeManagerController.java
+++ app/controllers/RecipeManagerController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import models.Difficulty;
 import models.Ingredient;
@@ -16,12 +19,12 @@ public class RecipeManagerController extends Controller {
         return ok("Recipe created!\n");
     }
 
-    public Result getAllRecipes(Http.Request request) {
-        return ok("Return of all the recipes\n");
-    } 
+    // public Result getAllRecipes(Http.Request request) {
+    //     return ok("Return of all the recipes\n");
+    // } 
 
-    public Result getRecipeByName(String name) {
-        return ok("Return of the recipe: " + name + "\n");
+    public Result getRecipeById(Long id) {
+        return ok("Return of the recipe: " + id + "\n");
     }
 
     public Result getRecipeByIngredients(List<Ingredient> ingredients) {
@@ -34,6 +37,27 @@ public class RecipeManagerController extends Controller {
 
     public Result getRecipesByRating(Rating rating) {
         return ok("Return of the recipes with rating: " + rating.getValue() + "\n");
+    }
+
+    public Result getRecipesByFilter(Http.Request request) {
+        Map<String, String[]> queryParamsMap = request.queryString();
+        String[] difficulty = queryParamsMap.get("difficulty");
+        String[] rating = queryParamsMap.get("rating");
+        String[] ingredients = queryParamsMap.get("ingredients");
+
+        if(difficulty != null) {
+            return ok("Return of the recipes filtered by difficulty " + Arrays.toString(difficulty) + "\n");
+        }
+        
+        if(rating != null) {
+            return ok("Return of the recipes filtered by rating " + Arrays.toString(rating) + "\n");
+        }
+
+        if(ingredients != null) {
+            return ok("Return of the recipes filtered by ingredients " + Arrays.toString(ingredients) + "\n");
+        }
+
+        return ok("Return of all the recipes\n");
     }
 
 }
